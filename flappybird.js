833d81c50e3e0882898df1beb065ecc11f78b649
Madhusudan_chauhan <madhusudanchauhan62@gmail.com>
2024-09-01 14:32:36 +0530

    build Basic Structure of the Game

---
+++ flappybird.js
@@ -0,0 +1,34 @@
+//board 
+let board;
+let boardWidth = 360;
+let boardHeight = 640;
+let context;
+
+//bird
+let birdWidth = 34;
+let birdHeight = 24;
+let birdX = boardWidth/8;
+let birdY = boardHeight/2;
+
+let bird ={
+    x : birdX,
+    y : birdY,
+    width : birdWidth,
+    height : birdHeight
+}
+
+
+
+
+window.onload = function(){
+    board = document.getElementById("board");
+    board.height = boardHeight;
+    board.width = boardWidth;
+    context= board.getContext("2d"); //used for drawing on the bomard
+
+    //draw flappy bird 
+    context.fillStyle = "green";
+    context.fillRect(bird.x , bird.y , bird.width ,bird.height);
+
+
+}
